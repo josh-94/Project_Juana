9edc4255ae61049376bde63736cf1b05fbedd7f0
Diego Morey <85264308+DAlons27@users.noreply.github.com>
2022-06-19 22:39:14 -0500

    Adding comments to functions

--- src/function_jwt.py
+++ src/function_jwt.py
@@ -4,16 +4,36 @@ from os import getenv
 from datetime import datetime, timedelta
 from flask import jsonify
 
+
 def expire_date(days: int):
+    """
+    Calcula la fecha de expiracion del token
+    :param days: Numero de dias que dura el token
+    :return: Fecha de expiracion del token
+    """
     now = datetime.now()
     new_date = now + timedelta(days)
     return new_date
 
+
 def write_token(data: dict):
-    token = encode(payload={**data, "exp": expire_date(2)}, key=getenv("SECRET"), algorithm="HS256")
-    return token.decode("UTF-8")
+    """
+    Escribe el token segun la configuracion de la aplicacion
+    :param data: Diccionario con los datos del usuario
+    :return: Token
+    """
+    token = encode(payload={**data, "exp": expire_date(2)},
+                   key=getenv("SECRET"), algorithm="HS256")
+    return token.encode("UTF-8")
+
 
 def validate_token(token, output=False):
+    """
+    Valida el token
+    :param token: Token
+    :param output: True si se quiere imprimir el token, False si no
+    :return: True si el token es valido, False si no
+    """
     try:
         if output:
             return decode(token, key=getenv("SECRET"), algorithms=["HS256"])
@@ -26,4 +46,5 @@ def validate_token(token, output=False):
         response = jsonify({"mensaje": "Token expirado"})
         response.status_code = 401
         return response
+
         
