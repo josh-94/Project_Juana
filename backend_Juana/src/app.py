fddde0e989d2b1de25244c3fff301fbe84b58584
Ruben Cadenas <ruben.cadenas@itland.pe>
2022-06-28 12:22:31 -0500

    update and use configuration file for dev and prod

--- backend_Juana/src/app.py
+++ backend_Juana/src/app.py
@@ -6,13 +6,16 @@ from config import config
 from routes.auth import routes_auth
 from routes.rutas import rutas
 from routes.ruta_auth import route_sharff
+from routes.test import test_blueprint
 from dotenv import load_dotenv
+from os import getenv
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 app.register_blueprint(routes_auth, url_prefix="/")
 app.register_blueprint(rutas, url_prefix="/")
 app.register_blueprint(route_sharff, url_prefix="/")
+app.register_blueprint(test_blueprint, url_prefix="/test")
 
 # La conexion para poder trabajar con la base de datos
 conexion = MySQL(app)   
@@ -40,16 +43,19 @@ def error_autorizacion(error):
 
 if __name__ == '__main__':
     # Recurre al archivo configuracion y toma el parametro DEBUG = TRUE
-    app.config.from_object(config['development'])
     load_dotenv()
     # Registro la funcion pagina_no_encontrada como pagina de error
     app.register_error_handler(404, pagina_no_encontrada)
     app.register_error_handler(405, error_autorizacion)
     # Inicia el servidor en el puerto 5000
-    isDev = getenv("ISDEV")
+    isDev = getenv("JNB_ISDEV")
     if (isDev == "TRUE"):
-        host = getenv("HOST", default="0.0.0.0")
-        port = getenv("PORT", default=5000)
-        app.run(debug=True, host=host, port=int(port))
+        app.config.from_object(config['development'])
+        #host = getenv("HOST", default="0.0.0.0")
+        #port = getenv("PORT", default=5000)
+        app.run(debug=True)
     else:
+        app.config.from_object(config['production'])
+        #host = getenv("HOST", default="0.0.0.0")
+        #port = getenv("PORT", default=5000)
         app.run(debug=False)
--- backend_Juana/src/config.py
+++ backend_Juana/src/config.py
@@ -1,16 +1,33 @@
+from os import getenv
+
 # Configuraciones del proyecto
 
 class DevelopmentConfig():
     # Modo de desarrollo
     DEBUG = True 
 
+    SERVER_NAME = getenv("JNB_SERVER_NAME", default="0.0.0.0:5000")
+
     # Credenciales para conectarse a la base de datos
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'Yawen123@'
-    MYSQL_DB = 'listproduct'
+    MYSQL_HOST = getenv("JNB_MYSQL_HOST", default="localhost") #''
+    MYSQL_USER = getenv("JNB_MYSQL_USER", default='root')
+    MYSQL_PASSWORD = getenv("JNB_MYSQL_PASSWORD", default='')
+    MYSQL_DB = getenv("JNB_MYSQL_DB", default='')
+
+class ProductionConfig():
+    # Modo de desarrollo
+    DEBUG = False 
+
+    #SERVER_NAME = getenv("JNB_SERVER_NAME", default="0.0.0.0:5000")
+
+    # Credenciales para conectarse a la base de datos
+    MYSQL_HOST = getenv("JNB_MYSQL_HOST", default="localhost") #''
+    MYSQL_USER = getenv("JNB_MYSQL_USER", default='root')
+    MYSQL_PASSWORD = getenv("JNB_MYSQL_PASSWORD", default='')
+    MYSQL_DB = getenv("JNB_MYSQL_DB", default='')
  
 config={
-    'development': DevelopmentConfig
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
 }
 
