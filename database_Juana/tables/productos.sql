82dc0b83dfcbfcaf344327b6bc093e39cdfec30e
josh.94 <jeshua.cabanillas@gmail.com>
2022-06-29 16:25:41 -0500

    update: script to create database and table with correct size

--- database_Juana/tables/productos.sql
+++ database_Juana/tables/productos.sql
@@ -1,13 +1,15 @@
+CREATE DATABASE IF NOT EXISTS listproduct;
+USE listproduct;
 CREATE TABLE productos(
     numeroGuia VARCHAR(25) NULL,
     numeroPedido VARCHAR(25) NULL,
-    estado VARCHAR(25) NULL,
-    lugar VARCHAR(25) NULL,
-    quienRecibe VARCHAR(25) NULL,
-    motivoDescripcion VARCHAR(25) NULL,
-    fecha VARCHAR(25) NULL,
-    hora VARCHAR(25) NULL,
-    link VARCHAR(25) NULL,
-    observacion VARCHAR(25) NULL,
+    estado VARCHAR(2) NULL,
+    lugar VARCHAR(100) NULL,
+    quienRecibe VARCHAR(20) NULL,
+    motivoDescripcion VARCHAR(100) NULL,
+    fecha VARCHAR(10) NULL,
+    hora VARCHAR(10) NULL,
+    link VARCHAR(400) NULL,
+    observacion VARCHAR(100) NULL,
     eliminado TINYINT(1) NOT NULL
 )
