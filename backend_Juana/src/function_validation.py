6729046c531a90c86f70de109102898eaf7d623f
Diego Morey <85264308+DAlons27@users.noreply.github.com>
2022-06-29 17:47:25 -0500

    Delete backend_Juana/src directory

--- backend_Juana/src/function_validation.py
+++
@@ -1,5 +0,0 @@
-def validar_nPedido(numeroPedido: str) -> bool:
-    """
-        Funcion que valida la cantidad de caracteres del numero de pedido debe ser 16
-        """
-    return len(numeroPedido) > 4
