303dcbb6b65628b3afdef1048b680c76d56a0fcf
mrgiulls <giuliano.mesias@hotmail.com>
2022-06-29 19:47:13 -0500

    Script of BD_juana

---
+++ database_Juana/tables/estado_pedidos.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE IF NOT EXISTS BD_juana
+USE BD_juana;
+CREATE TABLE IF NOT EXISTS estado_pedidos(
+    numeroGuia VARCHAR(25) NULL,
+    numeroPedido VARCHAR(25) NULL,
+    estado VARCHAR(2) NULL,
+    lugar VARCHAR(100) NULL,
+    quienRecibe VARCHAR(20) NULL,
+    motivoDescripcion VARCHAR(100) NULL,
+    fecha VARCHAR(10) NULL,
+    hora VARCHAR(10) NULL,
+    link VARCHAR(400) NULL,
+    observacion VARCHAR(100) NULL,
+    tiempo DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    eliminado TINYINT(1) NOT NULL,
+    id_estado_pedidos INT Primary key NOT NULL
+);
+
--- database_Juana/tables/pedido_request.sql
+++ database_Juana/tables/pedido_request.sql
@@ -1,5 +1,5 @@
-CREATE TABLE productos(
+CREATE TABLE pedido_request(
     header TEXT NOT NULL,
     content VARCHAR(800) NOT NULL,
     insert_datetime DATETIME NOT NULL
-)
+);
