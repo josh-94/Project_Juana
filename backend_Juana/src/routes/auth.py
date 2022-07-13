460f04bfc185960b7e17fb4b6a8e82163346aaa6
Diego Morey <85264308+DAlons27@users.noreply.github.com>
2022-07-13 12:24:16 -0500

    Update auth.py

--- backend_Juana/src/routes/auth.py
+++ backend_Juana/src/routes/auth.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 from flask_cors import CORS                             # El CORS debe estar en todos los archivos de ruta
 from function_jwt import write_token, validate_token
+from function_validation import validar_password
 
 routes_auth = Blueprint("routes_auth", __name__)
 CORS(routes_auth)                                       # importante para origen cruzado
@@ -12,8 +13,10 @@ def login():
     """
     data = request.get_json()
     if data['username'] == "UsuarioSharff":
-        if len(data['password']) > 7:
+        if validar_password(data['password']):
             return write_token(data=request.get_json())
+        else:
+            return jsonify({'Error': "La contraseña debe tener 8 digitos y no tener espacios."})
     else:
         response = jsonify({"message": "User not found"})
         response.status_code = 404
