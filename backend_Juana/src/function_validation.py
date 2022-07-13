0c2d8dc2964ece54ef42c0404959e11d6a5eef61
Diego Morey <85264308+DAlons27@users.noreply.github.com>
2022-07-13 12:23:07 -0500

    Update function_validation.py

--- backend_Juana/src/function_validation.py
+++ backend_Juana/src/function_validation.py
@@ -3,3 +3,17 @@ def validar_nPedido(numeroPedido: str) -> bool:
         Funcion que valida la cantidad de caracteres del numero de pedido debe ser 4
         """
     return len(numeroPedido) > 4
+
+def validar_password(password: str):
+    """
+        Funcion que valida la cantidad de caracteres del password debe ser 8
+        """
+    if len(password) > 7:
+        count = 0
+        for i in password:
+            if i.isspace() is True:
+                count += 1
+        if count == 0:
+            return True
+        else:
+            return False
